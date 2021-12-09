e62ace39078d3e8ccbb99b7570e333ee5af3c1e9
Sammy Gaskins <sammycole@comcast.net>
2021-12-09 10:31:37 -0600

    Revert "I'm not sure what these are"
    This reverts commit a5f4843d3e0392e613f7cf0f1030f87a20b2684b.

--- PorchSwingFarms/Pages/Index.cshtml.cs
+++ PorchSwingFarms/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PorchSwingFarms.Models;
+using PorchSwingFarms.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,7 +8,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.IO;
 
 namespace PorchSwingFarms.Pages
 {
@@ -23,6 +27,31 @@ namespace PorchSwingFarms.Pages
         public void OnGet()
         {
 
+        }
+
+        public ActionResult OnPostUpload(FileUpload fileUpload) {
+            foreach (var file in fileUpload.FormFiles)
+            {
+                using (var reader = new StreamReader(file.OpenReadStream()))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    csv.Read();
+                    csv.ReadHeader();
+                    while (csv.Read()) { 
+                        
+                    }
+                }
+            }
+            return Page();
+        }
+
+        //file Upload Class
+        public class FileUpload
+        {
+            [Required]
+            [Display(Name = "File")]
+            public List<IFormFile> FormFiles { get; set; } // convert to list
+            public string SuccessMessage { get; set; }
         }
     }
 
